69f4b34c5ae025977a72543abc9ba231c393a7a1
Immortal <rishitks1307@gmail.com>
2026-06-11 21:59:02 +0530

    New varaibles testing file

---
+++ variables.py
@@ -0,0 +1,4 @@
+a = str(input("Enter Your Name : "))
+b = int(input("Enter your class : "))
+c = str(input("Enter your section : "))
+print ("Name of the student is", a, "of class", b, "section", c)
